c3c89760470e6abca09c86add1b998e93ae20732
tim.rohrer <tim@itstechnical.net>
2020-01-09 21:25:54 +0000

    Tweaks

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Button({onClick, className ='', children}) {
+
+    return (
+        <button
+            onClick={onClick}
+            className={className}
+            type="button"
+        >
+            {children}
+        </button>
+    );
+}
+
+export default Button;
